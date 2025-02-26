4563e04e504571dc570121cd29d19bc3f0fc95f0
Steven Mattmann <steven_mattmann@sluz.ch>
2025-02-26 09:41:46 +0100

    refactor: change the style

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -7,6 +7,8 @@ import {
 } from '@angular/forms'
 import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card'
 import { MatFormField } from '@angular/material/form-field'
+import { MatCheckbox } from '@angular/material/checkbox'
+import { MatIcon } from '@angular/material/icon'
 
 @Component({
     selector: 'app-login',
@@ -19,15 +21,19 @@ import { MatFormField } from '@angular/material/form-field'
         MatCardContent,
         MatFormField,
         ReactiveFormsModule,
+        MatCheckbox,
+        MatIcon,
     ],
 })
 export class LoginComponent {
     loginForm: FormGroup
+    hidePassword = true
 
     constructor(private fb: FormBuilder) {
         this.loginForm = this.fb.group({
             email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(6)]],
+            password: ['', [Validators.required, Validators.minLength(8)]],
+            rememberMe: [false],
         })
     }
 
@@ -35,5 +41,9 @@ export class LoginComponent {
         if (this.loginForm.valid) {
             console.log('Form Data:', this.loginForm.value)
         }
+    }
+
+    togglePasswordVisibility() {
+        this.hidePassword = !this.hidePassword
     }
 }
