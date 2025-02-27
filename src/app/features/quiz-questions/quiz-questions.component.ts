47db8297a72ed0b14eaca28974135bf431a12df1
Nico Marcuard <Nico_Marcuard@sluz.ch>
2025-02-27 13:41:29 +0100

    feat: external css

--- src/app/features/quiz-questions/quiz-questions.component.ts
+++ src/app/features/quiz-questions/quiz-questions.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core'
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component'
+import { MatCardModule } from '@angular/material/card'
+import { MatIcon } from '@angular/material/icon'
+import { MatIconButton } from '@angular/material/button'
 
 @Component({
     selector: 'app-quiz-questions',
     standalone: true,
-    imports: [SidebarComponent],
+    imports: [SidebarComponent, MatCardModule, MatIcon, MatIconButton],
     templateUrl: './quiz-questions.component.html',
     styleUrl: './quiz-questions.component.scss',
 })
