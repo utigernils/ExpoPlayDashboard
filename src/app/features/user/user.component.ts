e01cf36a5b1781d963f71b0a6178accd8261e8f4
Nico Marcuard <Nico_Marcuard@sluz.ch>
2025-02-28 08:00:28 +0100

    feat: page layout and tables

--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component'
 import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatIconButton } from '@angular/material/button'
+
+export interface User {
+    id: number
+    name: string
+}
 
 @Component({
     selector: 'app-user',
     standalone: true,
-    imports: [SidebarComponent, MatCardModule],
+    imports: [
+        SidebarComponent,
+        MatCardModule,
+        CommonModule,
+        MatIconModule,
+        MatIconButton,
+    ],
     templateUrl: './user.component.html',
-    styleUrl: './user.component.scss',
+    styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {}
+export class UserComponent {
+    users: User[] = [
+        { id: 1, name: 'Nico Marcuard' },
+        { id: 2, name: 'Nils Utiger' },
+        { id: 3, name: 'Steven Mattmann' },
+    ]
+
+    trackByUser(index: number, user: User): number {
+        return user.id
+    }
+}
