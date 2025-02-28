db8471243e741f6f50977a93b4ebddb21947c77b
Steven Mattmann <steven_mattmann@sluz.ch>
2025-02-28 08:09:11 +0100

    refactor: solved problems

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
 import { Routes } from '@angular/router'
-import { LoginComponent } from './features/login/login.component'
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }]
+export const routes: Routes = [{}]
