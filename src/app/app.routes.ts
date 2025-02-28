64c2a5f49d6d26d9ccec6efd6248314144462e93
Steven Mattmann <steven_mattmann@sluz.ch>
2025-02-28 08:15:28 +0100

    refactor: solved problems

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router'
+import { DashboardComponent } from './features/dashboard/dashboard.component'
+import { ConsoleComponent } from './features/console/console.component'
+import { QuizQuestionsComponent } from './features/quiz-questions/quiz-questions.component'
+import { ExpoComponent } from './features/expo/expo.component'
+import { PlayerDataComponent } from './features/player-data/player-data.component'
+import { UserComponent } from './features/user/user.component'
+import { LoginComponent } from './features/login/login.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'quiz-questions', component: QuizQuestionsComponent },
+    { path: 'console', component: ConsoleComponent },
+    { path: 'expo', component: ExpoComponent },
+    { path: 'user', component: UserComponent },
+    { path: 'player', component: PlayerDataComponent },
+    { path: 'login', component: LoginComponent },
+]
