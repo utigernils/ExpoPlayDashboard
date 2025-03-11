7df9dc9fd407f822051aedda269b430a8f5557f8
utigernils <utigernils@gmail.com>
2025-03-11 08:12:20 +0100

    feat: header now supports s title field

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [],
     templateUrl: './header.component.html',
-    styleUrl: './header.component.scss',
+    styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    @Input() title: string = 'My Page Title' // Standardwert optional
+}
